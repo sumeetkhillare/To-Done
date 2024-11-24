917388b4ecf13570d930ad79d6ccdc69e6383619
shreddersk <sumeetkhillare10@gmail.com>
2024-11-24 17:33:26 -0500

    skhilla: Added model register

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import List, ListTags, ListItem, TemplateItem, SharedUsers, SharedList, Task
+
+admin.site.register(List)
+admin.site.register(ListTags)
+admin.site.register(ListItem)
+admin.site.register(TemplateItem)
+admin.site.register(SharedUsers)
+admin.site.register(SharedList)
+admin.site.register(Task)
