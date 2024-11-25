030d38dd956229add59215941eb4d41df2d3dfd6
xpincsu <xpi@ncsu.edu>
2024-11-25 15:13:08 -0500

    xpi : added template in admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import List, ListTags, ListItem, TemplateItem, SharedUsers, SharedList, Task
+from .models import List, ListTags, ListItem, TemplateItem, SharedUsers, SharedList, Task, Template
 
 admin.site.register(List)
 admin.site.register(ListTags)
 admin.site.register(ListItem)
 admin.site.register(TemplateItem)
+admin.site.register(Template)
 admin.site.register(SharedUsers)
 admin.site.register(SharedList)
 admin.site.register(Task)
